539d7841f4aba5bd41420010bf6680705e144472
François Delbrayelle <fdelbrayelle@gmail.com>
2025-09-09 11:15:33 +0200

    fix(): add missing category to AI

---
+++ src/main/java/io/kestra/plugin/anthropic/package-info.java
@@ -0,0 +1,8 @@
+@PluginSubGroup(
+    title = "Anthropic plugin",
+    description = "A plugin to use Anthropic.",
+    categories = PluginSubGroup.PluginCategory.AI
+)
+package io.kestra.plugin.anthropic;
+
+import io.kestra.core.models.annotations.PluginSubGroup;
